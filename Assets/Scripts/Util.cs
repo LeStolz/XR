72ba000f5aec5625725b657ce98173ced84a9e6b
LeStolz <vonamdang03@gmail.com>
2024-11-23 09:05:41 +0700

    Minor fixes

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -7,7 +7,7 @@ class Util
 
 	public static ulong GetTimeSinceEpoch()
 	{
-		DateTime epochStart = new(2024, 10, 1, 0, 0, 0, DateTimeKind.Utc);
+		DateTime epochStart = new(2024, 11, 1, 0, 0, 0, DateTimeKind.Utc);
 
 		return (ulong)(DateTime.UtcNow - epochStart).TotalMilliseconds;
 	}
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # XR
 
-- Save in the middle
-- Euphoria, enhance precision
-- Elevate FPV, try elevate projector
-- Fix assist
-- Nearer screen, offset from QR, new marker
-- Hide/show FPV, change sphere color
+- Make sure not occluded/occluding
+- Marker
+- Hide/show FPV
